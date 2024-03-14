53c7f0d30b40ab543c206970b70523c6ad03399f
Madhav sameer <111692125+Madhavsameer@users.noreply.github.com>
2024-03-14 10:15:41 +0530

    Update OnlineTutorialManagementApplication.java

--- src/main/java/com/application/OnlineTutorialManagementApplication.java
+++ src/main/java/com/application/OnlineTutorialManagementApplication.java
@@ -8,6 +8,7 @@ public class OnlineTutorialManagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineTutorialManagementApplication.class, args);
+		//hey
 	}
 
 }
